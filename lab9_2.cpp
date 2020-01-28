9b356369d960a70d85ae9b0c401bbe1da913e5dd
Apithep Piyaphiphathanamongkol <kaoapithepp@Apitheps-MacBook-Pro.local>
2020-01-28 15:23:23 +0700

    push "cheerbook_copy.txt"

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -2,3 +2,26 @@
 //cout << textline;
 //cout << "-------------------- SOTUS ---------------------";
 //Don't forget to change cout to output filestream
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    ifstream source;
+    ofstream dest ("cheerbook_copy.txt");
+    source.open("cheerbook.txt");
+    string textline;
+    dest << "-------------------- SOTUS ---------------------" << endl; 
+    while (getline(source,textline)){
+         dest << textline << endl;
+    }
+
+    dest << "-------------------- SOTUS ---------------------";
+    source.close();
+    dest.close();
+
+    return 0;
+}
