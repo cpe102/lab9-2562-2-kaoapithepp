dd007dc42fe270a3d79b3ae99cd62d912565ee81
Apithep Piyaphiphathanamongkol <kaoapithepp@Apitheps-MacBook-Pro.local>
2020-01-28 16:35:49 +0700

    todayIsSoooooooLuckyyyyyy

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -12,18 +12,19 @@ using namespace std;
 
 int main(){
     int count=0;
-    float sum=0;
+    float sum=0,eachSum;
     string textline;
 
     ifstream source("score.txt");
     while (getline(source,textline)){
+        eachSum += pow(atof(textline.c_str()),2);
         sum += atof(textline.c_str());
         count++;
     }
     cout << "Number of data = " << count << endl;
     float avg = sum/count;
     cout << "Mean = " << avg << endl;
-    float deviation = sqrt((pow(sum,2)/count)-pow(avg,2));
+    float deviation = sqrt((eachSum/count)-pow(avg,2));
     cout << "Standard deviation = " << deviation << endl;
 
     source.close();
